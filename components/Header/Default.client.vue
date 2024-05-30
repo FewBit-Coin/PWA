3f47ba68d2fb001c237e9fc4c71da3b90bb12376
hydromanx <xs19hydro@gmail.com>
2024-05-31 01:01:09 +0700

    update

--- components/Header/Default.client.vue
+++ components/Header/Default.client.vue
@@ -76,6 +76,11 @@ onUnmounted(() => {
 						<li v-if="!auth.isLoggedIn">
 							<NuxtLink
 								class="dark:hover:text-light_blue-50 hover:text-dark_blue-900 p-1"
+								to="https://fewnode.com"
+								target="_blank"
+							>{{ $t('content.signIn') }}</NuxtLink>
+							<NuxtLink
+								class="dark:hover:text-light_blue-50 hover:text-dark_blue-900 p-1"
 								:to="localePath('/login')"
 							>{{ $t('content.signIn') }}</NuxtLink>
 						</li>
